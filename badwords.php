6bdc7312652193beb0f1e8001eaaa0f213a84169
Gianluca <gl.galassini@gmail.com>
2021-07-24 19:02:03 +0200

    init e prova

---
+++ badwords.php
@@ -0,0 +1,20 @@
+<?php
+    $nome = "Monica";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words</title>
+</head>
+<body>
+    <h1>Ciao a tutte e tutti, mi chiamo <?php echo($nome) ?> </h1>
+</body>
+</html>
+
+
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a schermo il paragrafo e la sua lunghezza. Una parola da censurare viene passata dall'utente tramite parametro GET. Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+ -->
